ea78841bdcfcb9872f74c104c2a8ef939e04c4b1
dev <dev@develop.com>
2020-10-12 14:16:52 +0530

    cahgns

--- application/views/Order/orderstatusside.php
+++ application/views/Order/orderstatusside.php
@@ -31,8 +31,8 @@
             <i class="fa fa-question"></i> Other
         </a>
         <hr/>
-        <a class="btn btn-block btn-social btn-info"  href="<?php echo site_url("Order/orderRefund/$order_key");?>" target="_blank">
+<!--        <a class="btn btn-block btn-social btn-info"  href="<?php echo site_url("Order/orderRefund/$order_key");?>" target="_blank">
             <i class="fa fa-question"></i> Order Refund
-        </a>
+        </a>-->
     </div>
 </div>
