c364966075ae62ef7ba093f4b5751c04342bb341
octopuscart <dev@develop.com>
2021-01-01 08:21:18 +0530

    chagnes

--- application/views/couponManager/couponReportUsed.php
+++ application/views/couponManager/couponReportUsed.php
@@ -78,7 +78,7 @@ $this->load->view('layout/topmenu');
 
 <script>
     var apiurl = "https://manager2.woodlandshk.com/";
-    var apiurl = "http://localhost/woodlandcoupon/index.php/";
+//    var apiurl = "http://localhost/woodlandcoupon/index.php/";
 </script>
 
 <script src="<?php echo base_url(); ?>assets/plugins/DataTables/js/jquery.dataTables.js"></script>
